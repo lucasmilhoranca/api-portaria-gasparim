ed0680f469a6f72ebc24dad8f4734f0996e22a41
lucasmilhoranca <lucas1234milhoranca@gmail.com>
2025-05-23 14:08:30 -0300

    ajustes para aula de IO

--- src/routes/swagger.route.js
+++ src/routes/swagger.route.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 const route = Router();
 
 import swaggerUi from "swagger-ui-express";
-import swaggerDocument from "../swagger.json" assert { type: "json"};
+import swaggerDocument from '../swagger.json' with { type: 'json' };
 
 route.use("/", swaggerUi.serve);
 route.get("/", swaggerUi.setup(swaggerDocument));
